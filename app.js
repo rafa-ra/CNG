8e4755f2b957b774c9e76ca493926d424d1144dc
Rafael <reis.rafael18@gmail.com>
2023-07-10 18:07:35 -0300

    add multiple products

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("64ac68f7e661ad84c381d0a7")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
