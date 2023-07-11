1cd7accdc8d5178c966a70bcea98f4ffdc14b933
Rafael <reis.rafael18@gmail.com>
2023-07-11 19:49:48 -0300

    adminRoutes commented

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,35 @@
+//Importa módulo path para lidar com paths
 const path = require("path");
-
+//Importa Express.js
 const express = require("express");
-
+//Importa controllers administrativos
 const adminController = require("../controllers/admin");
-
+//Importa método de roteamento do Express.js
 const router = express.Router();
 
-// // /admin/add-product => GET
+//Define o método "getAddProduct" para "/add-product".
+//Rota para requisição da página de adição de produtos.
 router.get("/add-product", adminController.getAddProduct);
 
-// // /admin/products => GET
+//Define o método "getProducts" para requisições para "/products"
+//Rota para requisição da página de listagem de produtos.
 router.get("/products", adminController.getProducts);
 
-// // /admin/add-product => POST
+//Define o método "postAddProduct" para requisições para "/add-product"
+//Rota de adição de produtos
 router.post("/add-product", adminController.postAddProduct);
 
+//Define o método "getEditProduct" para requisições para "/edit-product/:productId"
+//Rota para requisição da página de edição de produtos.
 router.get("/edit-product/:productId", adminController.getEditProduct);
 
+//Define o método "postEditProduct" para requisições para "/edit-product"
+//Rota de edição de produtos
 router.post("/edit-product", adminController.postEditProduct);
 
+//Define o método "postDeleteProduct" para requisições para "/delete-product"
+//Rota de deleção de produtos
 router.post("/delete-product", adminController.postDeleteProduct);
 
+//Exporta as rotas
 module.exports = router;
