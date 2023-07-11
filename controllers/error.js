1ddac8ef37b846c3c3cfd47c81790f62e1a45145
Rafael <reis.rafael18@gmail.com>
2023-07-11 19:39:54 -0300

    error.js commented

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,7 @@
+//Exporta a rota '404'
 exports.get404 = (req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+  //Envia o status 404, de "página não encontrada".
+  //O método "render" renderiza a página do arquivo 404.ejs e
+  //passa pageTitle e path como parâmetros
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
